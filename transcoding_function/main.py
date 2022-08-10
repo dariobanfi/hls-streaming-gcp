902a8d04bcd7dbf26c6c8912e99dd976ccbe6549
Dario Banfi <dariobanfi@google.com>
2022-08-10 08:19:45 +0200

    Fixed output URI path

--- transcoding_function/main.py
+++ transcoding_function/main.py
@@ -16,7 +16,7 @@ def handle_gcs_event(cloud_event):
     project_id = "hls-streaming-gcp"
     region = "europe-west1"
     input_uri = f"gs://{bucket}/{name}"
-    output_uri = f"gs://hls-streaming-gcp-processed-files/{name}"
+    output_uri = f"gs://hls-streaming-gcp-processed-files-{project_id}/{name}/"
     preset = "preset/web-hd"
 
     client = TranscoderServiceClient()
