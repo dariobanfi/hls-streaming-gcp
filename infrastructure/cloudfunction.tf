7ddfdc467681ce3afdbf7b3396fe7f5ba3a0fd22
Dario Banfi <dariobanfi@google.com>
2022-08-10 11:52:34 +0200

    Added cloud function

--- infrastructure/cloudfunction.tf
+++ infrastructure/cloudfunction.tf
@@ -22,22 +22,22 @@ resource "google_storage_bucket_object" "zip" {
   source = data.archive_file.source.output_path
 }
 
-resource "google_eventarc_trigger" "upload" {
-  name     = "eventarc-upload-trigger"
-  location = var.region
-  matching_criteria {
-    attribute = "type"
-    value     = "google.cloud.storage.object.v1.finalized"
-  }
-  matching_criteria {
-    attribute = "bucket"
-    value     = google_storage_bucket.raw_files.name
-  }
-  destination {
-    workflow = google_workflows_workflow.process_file.id
-  }
-  service_account = google_service_account.transcoder_service_account.email
-}
+# resource "google_eventarc_trigger" "upload" {
+#   name     = "eventarc-upload-trigger"
+#   location = var.region
+#   matching_criteria {
+#     attribute = "type"
+#     value     = "google.cloud.storage.object.v1.finalized"
+#   }
+#   matching_criteria {
+#     attribute = "bucket"
+#     value     = google_storage_bucket.raw_files.name
+#   }
+#   destination {
+#     cloud_function = google_cloudfunctions2_function.function.id
+#   }
+#   service_account = google_service_account.transcoder_service_account.email
+# }
 
 resource "google_cloudfunctions2_function" "function" {
 
@@ -54,6 +54,7 @@ resource "google_cloudfunctions2_function" "function" {
         source {
             storage_source {
                 bucket = google_storage_bucket.cloudfunction_source.name
+                object = "${data.archive_file.source.output_md5}.zip"
             }
         }
     }
@@ -63,5 +64,16 @@ resource "google_cloudfunctions2_function" "function" {
         available_memory    = "256M"
         timeout_seconds     = 60
         service_account_email = google_service_account.transcoder_service_account.email
+    }
+
+    event_trigger {
+        trigger_region = var.region
+        event_type = "google.cloud.storage.object.v1.finalized"
+        retry_policy = "RETRY_POLICY_RETRY"
+        service_account_email = google_service_account.transcoder_service_account.email
+        event_filters {
+            attribute = "bucket"
+            value = google_storage_bucket.raw_files.name
+        }
     }
 }
